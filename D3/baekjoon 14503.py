a2300245b343c8c45fe7e0b5bf59b26d20d5b09b
DongyeopGu <gdy6650@gmail.com>
2020-02-17 22:39:40 +0900

    20.02.17 home

--- D3/baekjoon 14503.py
+++ D3/baekjoon 14503.py
@@ -1,31 +1,43 @@
-import sys
-sys.setrecursionlimit(30000)
+dt = [(-1,0),(0,1),(1,0),(0,-1)]   # 북, 동, 남, 서
+def leftdir(d):                    # 왼쪽 탐색 index 만들기 위해
+    if d == 0:
+        return 3
+    else:
+        return d - 1
 
-
-dt = [(0,-1),(1,0),(0,1),(-1,0)]   # 왼쪽부터 탐색위해
-def clean(x,y,d,check, cnt):
-    d = d%4
-    if room[x][y] == 0:
-        room[x][y] = 2
-        cnt += 1
-    nx, ny = x + dt[d][0], y + dt[d][0]
-    if room[nx][ny] == 0:
-        x = nx
-        y = ny
-        return clean(x,y,d+3,check,cnt)
-    elif (room[nx][ny] == 1 or room[nx][ny] == 2) and check <3:
-        return clean(nx,ny,d+3,check+1,cnt)
-    elif (room[nx][ny] == 1 or room[nx][ny] == 2) and check == 3:
-        if room[x+dt[(d+2)%4][0]][y+dt[(d+2)%4][1]] != 1:
-            nx += dt[(d+2)%4][0]
-            ny += dt[(d+2)%4][1]
-            return clean(nx,ny,d+3,0,cnt)
-        if room[x+dt[(d+2)%4][0]][y+dt[(d+2)%4][1]] == 1:
-            return cnt
 n, m = map(int,input().split())
 r, c, d = map(int, input().split())
 room = [list(map(int, input().split()))for _ in range(n)]
 check = 0
+x = r
+y = c
+while True:
+    d = d % 4
+    if check == 4:                          # 4가지 방향 탐색 후
+        bx = x - dt[d][0]          # 뒤로 한칸 가서
+        by = y - dt[d][1]          
+        if room[bx][by] == 1:               # 벽이면 break
+            break
+        else: 
+            x = bx
+            y = by
+            check = 0
+    if room[x][y] == 0:                     # 먼저 청소기 놓은곳 2로 변경
+        room[x][y] = 2
+    nd = leftdir(d)
+    nx = x + dt[nd][0]
+    ny = y + dt[nd][1]
+    if room[nx][ny] == 0:                   # 왼쪽이 0 일때 x, y 변경
+        x = nx
+        y = ny
+        d = leftdir(d)
+        check = 0
+    else:                 
+        d = leftdir(d)
+        check += 1
 cnt = 0
-result = clean(r,c,d,check,cnt)
-print(result)
+for i in range(n):
+    for j in range(m):
+        if room[i][j] == 2:
+            cnt += 1
+print(cnt)
