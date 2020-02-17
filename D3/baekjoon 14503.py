e13da5c9d77cff13698aa8a6bac6556acb913e08
DongyeopGu <gdy6650@gmail.com>
2020-02-17 19:25:41 +0900

    20.02.17

--- D3/baekjoon 14503.py
+++ D3/baekjoon 14503.py
@@ -1,22 +1,31 @@
-delta = [(-1,0),(0,1),(1,0),(0,-1)]
-g_l = [(0,-1),(1,0),(0,1),(-1,0)]   # 왼쪽부터 탐색위해
+import sys
+sys.setrecursionlimit(30000)
 
+
+dt = [(0,-1),(1,0),(0,1),(-1,0)]   # 왼쪽부터 탐색위해
+def clean(x,y,d,check, cnt):
+    d = d%4
+    if room[x][y] == 0:
+        room[x][y] = 2
+        cnt += 1
+    nx, ny = x + dt[d][0], y + dt[d][0]
+    if room[nx][ny] == 0:
+        x = nx
+        y = ny
+        return clean(x,y,d+3,check,cnt)
+    elif (room[nx][ny] == 1 or room[nx][ny] == 2) and check <3:
+        return clean(nx,ny,d+3,check+1,cnt)
+    elif (room[nx][ny] == 1 or room[nx][ny] == 2) and check == 3:
+        if room[x+dt[(d+2)%4][0]][y+dt[(d+2)%4][1]] != 1:
+            nx += dt[(d+2)%4][0]
+            ny += dt[(d+2)%4][1]
+            return clean(nx,ny,d+3,0,cnt)
+        if room[x+dt[(d+2)%4][0]][y+dt[(d+2)%4][1]] == 1:
+            return cnt
 n, m = map(int,input().split())
 r, c, d = map(int, input().split())
 room = [list(map(int, input().split()))for _ in range(n)]
-room[r][c] = 2  #현재 위치 청소표시
-
-while True:
-    x = r + g_l[d][0]
-    y = c + g_l[d][1]
-    d += 1
-    if d> 3:
-        d = 0
-    if room[x][y] == 0:
-        room[x][y] = 2
-    if (room[x][y] == 1 or room[x][y] == 2) and (room[x-delta[d][0]][y-delta[d][1]] == 0 or room[x-delta[d][0]][y-delta[d][1]] == 2):
-        x -= delta[d][0]
-        y -= delta[d][1]
-    if (room[x][y] == 1 or room[x][y] == 2) and room[x-delta[d][0]][y-delta[d][1]] == 1:
-        break
-print(room)    
+check = 0
+cnt = 0
+result = clean(r,c,d,check,cnt)
+print(result)
